9e19d1aa703d4d9e94078d951d2e6e18fd774219
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-04-24 02:24:46 +0200

    finished with bonus

--- CalcoliHelper.cs
+++ CalcoliHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,108 @@ namespace csharp_calcolatrice
 {
     public static class CalcoliHelper
     {
+        public static int SommaNumeri(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static double SommaNumeri(double num1, double num2)
+        {
+            return num1 + num2;
+        }
 
+        public static int DifferenzaNumeri(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double DifferenzaNumeri(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public static int MoltiplicaNumeri(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public static double MoltiplicaNumeri(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public static int ValoreAssoluto(int num)
+        {
+            return num = num < 0 ? (-num) : num;
+        }
+        public static double ValoreAssoluto(double num)
+        {
+            return num = num < 0 ? (-num) : num;
+        }
+        public static int NumeroMinore(int num1, int num2)
+        {
+            if (num1 < num2)
+                return num1;
+            else if (num2 < num1)
+                return num2;
+            else Console.WriteLine("I numeri sono uguali");
+            return 0;
+        }
+        public static double NumeroMinore(double num1, double num2)
+        {
+            if (num1 < num2)
+                return num1;
+            else if (num2 < num1)
+                return num2;
+            else Console.WriteLine("I numeri sono uguali");
+            return 0;
+        }
+        public static int NumeroMaggiore(int num1, int num2)
+        {
+            if (num1 < num2)
+                return num2;
+            else if (num2 < num1)
+                return num1;
+            else Console.WriteLine("I numeri sono uguali");
+            return 0;
+        }
+        public static double NumeroMaggiore(double num1, double num2)
+        {
+            if (num1 < num2)
+                return num2;
+            else if (num2 < num1)
+                return num1;
+            else Console.WriteLine("I numeri sono uguali");
+            return 0;
+        }
+        public static int ElevaAPotenza(int num1, int num2)
+        {
+            if (num1 == 0)
+            {
+                return 0;
+            }
+            //se esponente è zero
+            else if (num2 == 0 && num1 > 0) return 1;
+            else if (num2 == 0 && num1 < 0) return -1;
+            //se esponente è 1 
+            else if (num2 == 1 && num1 != 0) return num1;
+
+            else if (num1 != 0 && num2 > 0)
+            {
+                int result = 1;
+                for (int i = 0; i < num2; i++)
+                {
+                    result *= num1;
+                }
+
+                return result;
+            }
+            else
+            {
+                int result = 1;
+                for (int i = 0; i < num2; i++)
+                {
+                    result *= num1;
+                }
+                return (1 / result);
+            }
+        }
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            //Dover scrivere lo stesso metodo per tipi di parametro diversi applica il principio di OVERLOADING
+            Console.WriteLine(CalcoliHelper.SommaNumeri(3, 2));
+            Console.WriteLine(CalcoliHelper.SommaNumeri(3.7, 2.2));
+            Console.WriteLine(CalcoliHelper.DifferenzaNumeri(3, 2));
+            Console.WriteLine(CalcoliHelper.DifferenzaNumeri(3.7, 2.2));
+            Console.WriteLine(CalcoliHelper.MoltiplicaNumeri(3, 2));
+            Console.WriteLine(CalcoliHelper.MoltiplicaNumeri(3.7,2.2));
+            Console.WriteLine(CalcoliHelper.ValoreAssoluto(6));
+            Console.WriteLine(CalcoliHelper.ValoreAssoluto(-6));
+            Console.WriteLine(CalcoliHelper.NumeroMinore(3, 2));
+            Console.WriteLine(CalcoliHelper.NumeroMinore(3.7, 2.2));
+            Console.WriteLine(CalcoliHelper.NumeroMaggiore(3, 2));
+            Console.WriteLine(CalcoliHelper.NumeroMaggiore(3.7, 2.2));
+            Console.WriteLine(CalcoliHelper.ElevaAPotenza(2, 2));
+            Console.WriteLine(CalcoliHelper.ElevaAPotenza(0, 2));
+            Console.WriteLine(CalcoliHelper.ElevaAPotenza(2, 0));
+
         }
     }
 }
